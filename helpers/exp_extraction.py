e07db8ad9beca59fc73adc97b0aaf4a812806bc3
stamtron <stamzor25@gmail.com>
2020-08-07 15:36:41 +0100

    reducing number of extracted frames for long events

--- helpers/exp_extraction.py
+++ helpers/exp_extraction.py
@@ -2,7 +2,7 @@
 #sys.path.append('../helpers/')
 from helpers_frame_extraction import *
 
-def exposure_extraction(df, videos):
+def exposure_extraction(df, videos, path):
     codes = ['EXE','EXS','FJ','AN','SUS', 'SUE']
     df_new = df[df['Observation Code'].isin(codes)]
     df_new = df_new.reset_index(drop=True)
@@ -22,65 +22,65 @@ def exposure_extraction(df, videos):
                 ch3_video = video_paths[i]
         if (df_new['offset_Ch1'][k] < df_new['offset_Ch1'][k+1]):
             if (df_new['Observation Code'][k] == 'EXS') and (df_new['Observation Code'][k+1] == 'EXE' or df_new['Observation Code'][k+1] == 'SUS'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_with_sns(str(ch2_video), path_to_save, k, df_new, 2, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_with_sns(str(ch1_video), path_to_save, k, df_new, 1, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_with_sns(str(ch3_video), path_to_save, k, df_new, 3, 'up')
                 # works fine
             if (df_new['Observation Code'][k] == 'EXS') and (df_new['Observation Code'][k+1] == 'AN' or df_new['Observation Code'][k+1] == 'FJ'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_special_cases_after_startend(str(ch2_video), path_to_save, k, df_new, 2, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_special_cases_after_startend(str(ch1_video), path_to_save, k, df_new, 1, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_special_cases_after_startend(str(ch3_video), path_to_save, k, df_new, 3, 'up')  
                 # works fine
             if (df_new['Observation Code'][k] == 'FJ' or df_new['Observation Code'][k] == 'AN') and (df_new['Observation Code'][k+1] == 'AN' or df_new['Observation Code'][k+1] == 'FJ'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_between_fjan(str(ch2_video), path_to_save, k, df_new, 2, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_between_fjan(str(ch1_video), path_to_save, k, df_new, 1, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_between_fjan(str(ch3_video), path_to_save, k, df_new, 3, 'up')
             if (df_new['Observation Code'][k] == 'FJ' or df_new['Observation Code'][k] == 'AN') and (df_new['Observation Code'][k+1] == 'SUS' or df_new['Observation Code'][k+1] == 'EXE'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_special_cases_after_fjan(str(ch2_video), path_to_save, k, df_new, 2, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_special_cases_after_fjan(str(ch1_video), path_to_save, k, df_new, 1, 'up')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_special_cases_after_fjan(str(ch3_video), path_to_save, k, df_new, 3, 'up')
         else:
             if (df_new['Observation Code'][k+1] == 'EXS') and (df_new['Observation Code'][k] == 'EXE' or df_new['Observation Code'][k] == 'SUS'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_with_sns(str(ch2_video), path_to_save, k, df_new, 2, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_with_sns(str(ch1_video), path_to_save, k, df_new, 1, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_with_sns(str(ch3_video), path_to_save, k, df_new, 3, 'down')
                 # works fine
             if (df_new['Observation Code'][k+1] == 'EXS') and (df_new['Observation Code'][k] == 'AN' or df_new['Observation Code'][k] == 'FJ'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_special_cases_after_startend(str(ch2_video), path_to_save, k, df_new, 2, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_special_cases_after_startend(str(ch1_video), path_to_save, k, df_new, 1, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_special_cases_after_startend(str(ch3_video), path_to_save, k, df_new, 3, 'down')
                 # works fine
             if (df_new['Observation Code'][k] == 'FJ' or df_new['Observation Code'][k] == 'AN') and (df_new['Observation Code'][k+1] == 'AN' or df_new['Observation Code'][k+1] == 'FJ'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_between_fjan(str(ch2_video), path_to_save, k, df_new, 2, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_between_fjan(str(ch1_video), path_to_save, k, df_new, 1, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_between_fjan(str(ch3_video), path_to_save, k, df_new, 3, 'down')
             if (df_new['Observation Code'][k+1] == 'FJ' or df_new['Observation Code'][k+1] == 'AN') and (df_new['Observation Code'][k] == 'EXE' or df_new['Observation Code'][k+1] == 'SUS'):
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/centre_Ch2/exp/'
+                path_to_save = path + 'centre_Ch2/exp/'
                 frame_extraction_special_cases_after_startend(str(ch2_video), path_to_save, k, df_new, 2, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/left_port_Ch1/exp/'
+                path_to_save = path + 'left_port_Ch1/exp/'
                 frame_extraction_special_cases_after_startend(str(ch1_video), path_to_save, k, df_new, 1, 'down')
-                path_to_save = '/media/scratch/astamoulakatos/nsea_frame_test/right_starboard_Ch3/exp/'
+                path_to_save = path + 'right_starboard_Ch3/exp/'
                 frame_extraction_special_cases_after_startend(str(ch3_video), path_to_save, k, df_new, 3, 'down')
 
 
