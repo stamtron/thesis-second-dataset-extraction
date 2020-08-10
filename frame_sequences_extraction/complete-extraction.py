ae5026cf499c6f5b901f334ff820e84397034d29
stamtron <stamzor25@gmail.com>
2020-08-10 13:22:05 +0100

    data statistics and stratification

--- frame_sequences_extraction/complete-extraction.py
+++ frame_sequences_extraction/complete-extraction.py
@@ -7,7 +7,7 @@ from fj_and_extraction import *
 from bur_extraction import *
 from exp_extraction import *
 
-csv_path = Path('../csvs_with_startends/')
+csv_path = Path('../csv_for_frame_extraction/')
 
 csv_paths = list(csv_path.glob('*'))
 
@@ -15,8 +15,7 @@ folder_path = Path('/media/data/astamoulakatos/Survey-2-2012/Project 1/IC2')
 
 folders_path = list(folder_path.glob('*'))
 
-folders_path = [folders_path[9], folders_path[3], folders_path[2]]
-
+folders_path = [folders_path[9], folders_path[0], folders_path[5], folders_path[1], folders_path[10], folders_path[4], folders_path[7], folders_path[3], folders_path[8], folders_path[2]]
 for c, vf in zip(csv_paths, folders_path):
     print(c , vf)
     
@@ -35,15 +34,15 @@ for c, vf in zip(csv_paths, folders_path):
             print('DataFrame is empty!')
         else:
             codes  = df_vid['Observation Code'].unique()
-            if 'FJS' or 'ANS' in codes:
-                df_vid = add_event_for_start(df_vid)
-                vid_path = vf/f
-                videos = list(vid_path.glob('*'))
-                df_vid = add_event_for_end(df_vid, videos)
-                #print(videos)
-                counter += 1
-                print(counter)
-                suspension_extraction(df_vid, videos, path)
-                fieldjoint_anode_extraction(df_vid, videos, path)
-                burial_extraction(df_vid, videos, path)
-                exposure_extraction(df_vid, videos, path)
+#             if 'FJS' or 'ANS' in codes:
+            df_vid = add_event_for_start(df_vid)
+            vid_path = vf/f
+            videos = list(vid_path.glob('*'))
+            df_vid = add_event_for_end(df_vid, videos)
+            #print(videos)
+            counter += 1
+            print(counter)
+            suspension_extraction(df_vid, videos, path)
+            fieldjoint_anode_extraction(df_vid, videos, path)
+            #burial_extraction(df_vid, videos, path)
+            #exposure_extraction(df_vid, videos, path)
