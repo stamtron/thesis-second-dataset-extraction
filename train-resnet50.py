c09fd2f5561adee6e3a4c44aaf303e3181725e82
stamtron <stamzor25@gmail.com>
2020-10-05 11:43:56 +0100

    added metrics for labels in the train loop

--- train-resnet50.py
+++ train-resnet50.py
@@ -13,12 +13,12 @@ head = Head()
 resnet.avgpool = adaptive_pooling
 resnet.fc = head
 
-os.environ['CUDA_VISIBLE_DEVICES']='0,1'
+os.environ['CUDA_VISIBLE_DEVICES']='0,1,2'
 
 resnet = resnet.cuda()
 
 for param in resnet.parameters():
-    param.requires_grad = True
+    param.requires_grad = False
     
 for param in resnet.avgpool.parameters():
     param.requires_grad = True
@@ -26,37 +26,44 @@ for param in resnet.avgpool.parameters():
 for param in resnet.fc.parameters():
     param.requires_grad = True
 
-#resnet = nn.DataParallel(resnet)
+resnet = nn.DataParallel(resnet)
+check_freeze(resnet.module)
 
 #summary(resnet.module, torch.zeros(2,3,576,704).cuda())
 
-tensor_transform = get_tensor_transform('ImageNet')
+tensor_transform = get_tensor_transform('ImageNet', True)
 train_spat_transform = get_spatial_transform(2)
 train_temp_transform = get_temporal_transform()
 valid_spat_transform = get_spatial_transform(0)
 valid_temp_transform = va.TemporalFit(size=16)
 
-df = pd.read_csv('./important_csvs/events_with_number_of_frames_stratified.csv')
-df = get_df(df, 50, False, True, False)
-class_image_paths, end_idx = get_indices(df)
-train_loader = get_loader(1, 8, end_idx, class_image_paths, train_temp_transform, train_spat_transform, tensor_transform, False, True)
-df = pd.read_csv('./important_csvs/events_with_number_of_frames_stratified.csv')
-df = get_df(df, 50, False, False, True)
-class_image_paths, end_idx = get_indices(df)
-valid_loader = get_loader(1, 8, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
+root_dir = '/media/scratch/astamoulakatos/nsea_video_jpegs/'
+df = pd.read_csv('./small_dataset_csvs/events_with_number_of_frames_stratified.csv')
+df_train = get_df(df, 20, True, False, False)
+class_image_paths, end_idx = get_indices(df_train, root_dir)
+train_loader = get_loader(1, 270, end_idx, class_image_paths, train_temp_transform, train_spat_transform, tensor_transform, False, True)
+df_valid = get_df(df, 20, False, True, False)
+class_image_paths, end_idx = get_indices(df_valid, root_dir)
+valid_loader = get_loader(1, 270, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
+df_test = get_df(df, 20, False, False, True)
+class_image_paths, end_idx = get_indices(df_test, root_dir)
+test_loader = get_loader(1, 270, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
 
 torch.cuda.empty_cache()
 
-lr = 5e-2
-optimizer = optim.Adam(resnet.parameters(), lr=lr, weight_decay=1e-2)
+lr = 1e-2
+epochs = 10
+optimizer = optim.AdamW(resnet.parameters(), lr=lr, weight_decay=1e-2)
 criterion = nn.BCEWithLogitsLoss()
-scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, steps_per_epoch=len(train_loader), epochs=6)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, steps_per_epoch=len(train_loader), epochs=epochs)
+
 dataloaders = {
     "train": train_loader,
     "validation": valid_loader
 }
+
 save_model_path = '/media/raid/astamoulakatos/saved-resnet-models/'
 device = torch.device('cuda')
-writer = SummaryWriter('runs/ResNet3D_experiment')
-train_model_yo(save_model_path, dataloaders, device, resnet, criterion, optimizer, scheduler, writer, num_epochs=6)
+writer = SummaryWriter('runs/ResNet2D_vol2')
+train_model_yo(save_model_path, dataloaders, device, resnet, criterion, optimizer, scheduler, writer, epochs)
 writer.close()
