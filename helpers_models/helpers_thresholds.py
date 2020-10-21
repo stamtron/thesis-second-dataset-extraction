dcb9c54edec36c8b6142b6cd914ca9f1ad3b23c1
stamtron <stamzor25@gmail.com>
2020-10-21 10:52:05 +0100

    plot pr curves disgusting

--- helpers_models/helpers_thresholds.py
+++ helpers_models/helpers_thresholds.py
@@ -5,6 +5,13 @@ from sklearn.metrics import average_precision_score, precision_recall_curve, acc
 from sklearn.metrics import multilabel_confusion_matrix, precision_recall_fscore_support
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+
+plt.rcParams['figure.figsize'] = (20,8)
+font = {'family' : 'DejaVu Sans', 'weight' : 'normal', 'size'  : 22}
+plt.rc('font', **font)
+
 
 # we assume that y contains a tuple of y_pred and targets
 def nsea_compute_thresholds(y_true, y_pred, classes):
@@ -16,8 +23,6 @@ def nsea_compute_thresholds(y_true, y_pred, classes):
     for i in range(5):
         precisions[i], recalls[i], Thresholds[i] = precision_recall_curve(y_true[:, i], y_pred[:, i])
 
-    
-    
     result = {}
     ###############
     ###############  FIX THE UGLY STAFF BELOW
@@ -92,3 +97,57 @@ def write_dictionary_to_csv(csv_file, res):
             
 def truncate_decimals(arr, places=3):
     return np.floor(arr*(10**places))/(10**places)
+
+
+
+
+# dist = [ np.sqrt((1-re)**2 + (1-pre)**2) for re, pre in zip(re, pre) ]
+# opt_id.append(dist.index(min(dist)))
+# t = Thresholds[i]
+# opt_thres = t[opt_id[i]]
+# result[event_type] = opt_thres
+
+
+def plot_pr_curves(first_lim, y_true, y_pred, classes):
+    precisions = dict()
+    recalls = dict()
+    Thresholds = dict()
+    for i in range(5):
+        precisions[i], recalls[i], Thresholds[i] = precision_recall_curve(y_true[:, i], y_pred[:, i])
+    
+    opt_id = []
+    for i in range(5): 
+        re = recalls[i]
+        pre = precisions[i]
+        dist = [ np.sqrt((1-re)**2 + (1-pre)**2) for re, pre in zip(re, pre) ]
+        opt_id.append(dist.index(min(dist)))
+        t = Thresholds[i]
+        opt_thres = t[opt_id[i]]
+        f1_score_opt = 2.0*re[opt_id[i]]*pre[opt_id[i]] / (re[opt_id[i]]+pre[opt_id[i]])
+        print(re[opt_id[i]], pre[opt_id[i]], f1_score_opt)
+        print("Optimal ",classes[i]," Threshold = ", opt_thres )
+        print()
+
+    cmaps = OrderedDict()
+    plt.figure(figsize=(12,8))
+    for i in range(5): 
+        plt.plot(recalls[i], precisions[i], label = classes[i], linewidth=3.0)
+        #plt.plot(recalls[i][3:-4], precisions[i][3:-4], label = classes[i], linewidth=3.0)
+        plt.plot([recalls[i][opt_id[i]],1],[precisions[i][opt_id[i]],1], 'ro--')
+        plt.scatter(recalls[i][opt_id[i]],precisions[i][opt_id[i]], marker='*', s=900)
+        plt.text(0.85, 0.8, 'opt. threshold', color='r', size=18)
+    plt.xlabel('Recall', size=20)
+    plt.ylabel('Precision', size=20)
+    plt.xlim(first_lim,1.05)
+    plt.ylim(first_lim,1.05)
+    plt.title("Precision Recall Curves")
+    #plt.legend()
+    #plt.axis('off')
+    #plt.axis('equal')
+    #plt.gca().set_aspect('equal')
+    plt.savefig('figures/precision_recall_curve_zoom.png', dpi=300)
+    plt.show()    
+
+    
+def table_to_latex(table):
+    print(table.to_latex(float_format=lambda x: '%.3f' % truncate_decimals(x,3)))
--- train-resnet50.py
+++ train-resnet50.py
@@ -34,7 +34,7 @@ check_freeze(resnet.module)
 
 #summary(resnet.module, torch.zeros(2,3,576,704).cuda())
 
-tensor_transform = get_tensor_transform('ImageNet', True)
+tensor_transform = get_tensor_transform('ImageNet', False)
 train_spat_transform = get_spatial_transform(2)
 train_temp_transform = get_temporal_transform()
 valid_spat_transform = get_spatial_transform(0)
@@ -75,7 +75,7 @@ dataset = MyDataset(
         oned = True)
 train_loader = DataLoader(
         dataset,
-        batch_size = 120,
+        batch_size = 100,
         sampler = train_sampler,
         drop_last = True,
         num_workers = 0)
@@ -85,21 +85,27 @@ train_loader = DataLoader(
 #######################################################################################
 df_valid = get_df(df, 50, False, True, False)
 class_image_paths, end_idx = get_indices(df_valid, root_dir)
-valid_loader = get_loader(1, 120, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
+valid_loader = get_loader(1, 100, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
 df_test = get_df(df, 50, False, False, True)
 class_image_paths, end_idx = get_indices(df_test, root_dir)
-test_loader = get_loader(1, 120, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
+test_loader = get_loader(1, 50, end_idx, class_image_paths, valid_temp_transform, valid_spat_transform, tensor_transform, False, True)
 
 torch.cuda.empty_cache()
 
 load = True
 if load:
-    checkpoint = torch.load('/media/scratch/astamoulakatos/saved-resnet-models/third-round-unfreezed/best-checkpoint-013epoch.pth')
+    checkpoint = torch.load('/media/scratch/astamoulakatos/saved-resnet-models/forth-round-unfreezed/best-checkpoint-011epoch.pth')
     resnet.load_state_dict(checkpoint['model_state_dict'])
     print('loading pretrained freezed model!')
     
     for param in resnet.module.parameters():
+        param.requires_grad = False
+        
+    for param in resnet.module.avgpool.parameters():
         param.requires_grad = True
+    
+    for param in resnet.module.fc.parameters():
+        param.requires_grad = True    
         
     check_freeze(resnet.module)
 
@@ -122,13 +128,13 @@ if load:
     epochs = 15
     optimizer = optim.AdamW(resnet.parameters(), lr=lr, weight_decay=1e-2)
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    lr = 5e-4
+    lr = 5e-3
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.000001, patience=3)
     
 save_model_path = '/media/scratch/astamoulakatos/saved-resnet-models/'
 device = torch.device('cuda')
-writer = SummaryWriter('runs/ResNet2D_focal_loss_balanced_sampler_unfreezed_more_train')
+writer = SummaryWriter('runs/ResNet2D_focal_loss_balanced_sampler_full_res')
 train_model_yo(save_model_path, dataloaders, device, resnet, criterion, optimizer, scheduler, writer, epochs)
 writer.close()
